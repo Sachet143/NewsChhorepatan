837804d5da5e5956e1f83bd54f961625c8c7a68b
Sachet Shrestha <revenger.stha@gmail.com>
2023-07-03 12:25:29 +0545

    used fetch api

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,14 +8,15 @@ export class NewsItem extends Component {
       <div className="my-3">
         <div className="card">
           <img
-            src={imgUrl}
+            src={imgUrl?imgUrl:"https://tutorialslink.com/images/default-news-image.png"}
             className="card-img-top"
+            style={{width: '286px', height:'181px'}}
             alt="..."
           />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">
+            <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">
               Read More
             </a>
           </div>
